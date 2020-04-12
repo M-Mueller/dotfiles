86f2551c914450fe794ddd5013c1391f4959195e
M-Mueller <M-Mueller@users.noreply.github.com>
2020-04-12 14:24:45 +0200

    Add elm ctags

--- makelinks.sh
+++ makelinks.sh
@@ -4,9 +4,8 @@ DOTFILES=`pwd`
 
 cd ~
 ln -s $DOTFILES/.gitconfig
+ln -s $DOTFILES/.ctags
 ln -s $DOTFILES/nvim .config
-ln -s $DOTFILES/zsh .config
-ln -s $DOTFILES/.zshrc
 ln -s $DOTFILES/fish .config
 
 cd $DOTFILES
