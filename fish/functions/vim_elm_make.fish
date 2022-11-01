92e4adeb46910c1267386befad98d812c74c42bf
M-Mueller <M-Mueller@users.noreply.github.com>
2022-11-01 21:51:16 +0100

    Remove unused function

--- fish/functions/vim_elm_make.fish
+++
@@ -1,3 +0,0 @@
-function vim_elm_make
-    elm make --report=json --output=/dev/null src/Main.elm &| jq -j '.errors[] as $e | ($e.problems[] | $e.path, ":", .region.start.line, ":", .region.start.column,  " (", $e.name , ") ", .title, "\n")'
-end
